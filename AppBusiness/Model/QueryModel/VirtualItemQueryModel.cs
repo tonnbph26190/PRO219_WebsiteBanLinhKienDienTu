171afc2bdf9b15d8d54b2800326f88724ff8c1f4
vuh111 <vuh1119@gmail.com>
2024-06-12 19:08:16 +0700

    newa

--- AppBusiness/Model/QueryModel/VirtualItemQueryModel.cs
+++ AppBusiness/Model/QueryModel/VirtualItemQueryModel.cs
@@ -4,10 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AppData.IRepositories
+namespace AppBusiness.Model.QueryModel
 {
-    public interface IVW_Phone
+    public class VirtualItemQueryModel
     {
-
     }
 }
